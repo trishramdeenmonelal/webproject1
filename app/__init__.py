1cc83318cac7ba7881aa2f5434d462c0901ff225
trishramdeenmonelal <shawn.ramdeen16@gmail.com>
2020-03-13 20:38:13 -0500

    Project 1 Complete :)

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secretkey"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:password123@localhost/webproj1"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://dsygsoqfwyqhul:70990c4f47b06c82028491d14bca5ec78ab59c5158549754938dfd219b54ad5d@ec2-52-73-247-67.compute-1.amazonaws.com:5432/dd8ngic6cd3b7n"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['UPLOAD_FOLDER']="./app/static/uploads"
 
